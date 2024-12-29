c4461761214ce9e5f0776aacb46f88663ea55f2b
Kairee G <89224268+Kaireega@users.noreply.github.com>
2024-12-28 20:34:55 -0500

    add all files excpet frontend

---
+++ src/main/java/com/Jiraiya_sen/solution/JiraiyaSenSolutionApplication.java
@@ -0,0 +1,13 @@
+package com.Jiraiya_sen.solution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiraiyaSenSolutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JiraiyaSenSolutionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Jiraiya_sen/solution/JiraiyaSenSolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Jiraiya_sen.solution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiraiyaSenSolutionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
